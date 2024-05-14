6fa0a80d31e234000255ce75240aad927d16d448
Nithin7349 <nithinyashu846@gmail.com>
2024-05-14 11:40:03 +0530

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${(index - slideIndex) * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+}
+
+// Automatic sliding every 3 seconds
+
